a8cea6fffd5d37f4eb689091cdc13ba717ee7fa1
Emre Uslu <75951256+rakun256@users.noreply.github.com>
2024-09-30 01:27:36 +0300

    first commit

---
+++ wheelData.js
@@ -0,0 +1,9 @@
+function getWheelData() {
+    return [
+        { value: "Çanta", color: "#db7093", percent: 0.2 }, 
+        { value: "Redbull", color: "#20b2aa", percent: 0.2 },  
+        { value: "Defter", color: "#daa520", percent: 0.2 }, 
+        { value: "Kalem", color: "#ff340f", percent: 0.2 }, 
+        { value: "Anahtarlık", color: "#4169e1", percent: 0.2 }   
+    ];
+}
