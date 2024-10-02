b03a4574d378c75bd248d9990b62683f67bef668
Emre Uslu <75951256+rakun256@users.noreply.github.com>
2024-10-02 12:24:01 +0300

    yeter

--- wheelData.js
+++ wheelData.js
@@ -1,9 +1,9 @@
 function getWheelData() {
     return [
-      { value: " Sisal Çanta", color: "#db7093", percent: 15 }, // 50% şans
-      { value: "Redbull", color: "#20b2aa", percent: 10 },  // 10% şans
-      { value: "Defter", color: "#daa520", percent: 25 },   // 10% şans
-      { value: "Kalem", color: "#ff340f", percent: 40 },    // 10% şans
-      { value: "Anahtarlık", color: "#4169e1", percent: 10 } // 20% şans
+      { value: " Sisal Çanta", color: "#db7093", percent: 10 }, // 50% şans
+      { value: "Redbull", color: "#20b2aa", percent: 15 },  // 10% şans
+      { value: "Kalem", color: "#ff340f", percent:30 },   // 10% şans
+      { value: "Kalem", color: "#ff340f", percent: 30 },    // 10% şans
+      { value: "Anahtarlık", color: "#4169e1", percent: 15 } // 20% şans
     ];
 }
